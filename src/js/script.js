ba680aaa256ed83f61031b5c51b0a4ddb5b0b42b
kor1k <stanislav@bugfreakmedia.com>
2019-05-15 09:05:28 +0300

    added forms: name, email, pass1, pass2, checkbox and active button 'Продолжить'

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,44 @@
-function checkPass()
-{
-    var pass1 = document.getElementById('pass1');
-    var pass2 = document.getElementById('pass2');
+function checkPass() {
+    var pass1 = document.getElementById('form_pass1');
+    var pass2 = document.getElementById('form_pass2');
     var message = document.getElementById('error-nwl');
     var goodColor = "#66cc66";
     var badColor = "#ff6666";
 
-    if(pass1.value.length > 5)
-    {
+    if (pass1.value.length > 5) {
         pass1.style.backgroundColor = goodColor;
         message.style.color = goodColor;
         message.innerHTML = "character number ok!"
-    }
-    else
-    {
+    } else {
         pass1.style.backgroundColor = badColor;
         message.style.color = badColor;
-        message.innerHTML = " you have to enter at least 6 digit!"
+        message.innerHTML = " you have to enter at least 6 digit!";
         return;
     }
 
-    if(pass1.value === pass2.value)
-    {
+    if (pass1.value === pass2.value) {
         pass2.style.backgroundColor = goodColor;
         message.style.color = goodColor;
-        message.innerHTML = "ok!"
-    }
-    else
-    {
+        message.innerHTML = "ok!";
+    } else {
         pass2.style.backgroundColor = badColor;
         message.style.color = badColor;
-        message.innerHTML = " These passwords don't match"
+        message.innerHTML = " These passwords don't match";
     }
 }
+
+$('#form-fields__inputs input').on('click keyup', function () {
+        console.log($('#form_checkbox').is(':checked'));
+        if ($('#form_name').val() !== "" && $('#form_email').val() !== "" && $('#form_checkbox').is(':checked') == true && $('#form_pass1').val().length >= 6 && $('#form_pass2').val().length >= 6) {
+            console.log($('#form_checkbox').is(':checked'));
+            $('.content-side-form__form-submit-btn').addClass('active');
+            $('.form-checker').removeClass('active');
+            $('#reg-form-submit').click(function () {
+                $(this).location.reload(true);
+            })
+        } else {
+            $('.content-side-form__form-submit-btn').removeClass('active');
+            $('.form-checker').addClass('active');
+        }
+    }
+);
